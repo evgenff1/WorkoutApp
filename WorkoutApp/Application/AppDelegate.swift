e3fea0b1dbd6458c0dbcbd18dd9a4272217509ef
evgenff1 <139433911+evgenff1@users.noreply.github.com>
2024-03-06 20:10:06 +0300

    Remove: Delete Main.storyboard

---
+++ WorkoutApp/Application/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  WorkoutApp
+//
+//  Created by Evgeniy Fakhretdinov on 06.03.2024.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
---
+++ WorkoutApp/Application/SceneDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  SceneDelegate.swift
+//  WorkoutApp
+//
+//  Created by Evgeniy Fakhretdinov on 06.03.2024.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+
+        let tabBarController = UITabBarController()
+
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene
+        window?.rootViewController = tabBarController
+        window?.makeKeyAndVisible()
+    }
+
+
+
+}
+
