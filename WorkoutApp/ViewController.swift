e58be418dff72ff0b1ffd24fb9ce335a8e1713fb
evgenff1 <139433911+evgenff1@users.noreply.github.com>
2024-03-06 17:30:27 +0300

    Initial Commit

---
+++ WorkoutApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WorkoutApp
+//
+//  Created by Evgeniy Fakhretdinov on 06.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
