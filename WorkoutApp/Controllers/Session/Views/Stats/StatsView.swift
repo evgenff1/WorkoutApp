d8422fd6d7ed77adb0dae76c4ad18c3dd3fbae07
evgenff1 <139433911+evgenff1@users.noreply.github.com>
2024-03-22 15:10:44 +0300

    Finish statsView

--- WorkoutApp/Controllers/Session/Views/Stats/StatsView.swift
+++ WorkoutApp/Controllers/Session/Views/Stats/StatsView.swift
@@ -9,22 +9,42 @@ import UIKit
 
 final class StatsView: WABaseInfoView {
     
-    private let itemView = StatsItemView()
+    private let stackView: UIStackView = {
+        let view = UIStackView()
+        view.axis = .vertical
+        view.spacing = 15
+        return view
+    }()
     
-    override func setupViews() {
-        super.setupViews()
-        
-        setupView(itemView)
-        itemView.cunfigure(with: StatsItemView.StatsItem(
-            image: R.Images.Session.Stats.heartRate,
-            value: "155 bpm",
-            title: R.Strings.Session.heartRate.uppercased()
-        ))
-        
-        NSLayoutConstraint.activate([
-            itemView.topAnchor.constraint(equalTo: topAnchor, constant: 45),
-            itemView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 15)
-        ])
+    func configure(with itmes: [StatsItem]) {
+        itmes.forEach {
+            let itemView = StatsItemView()
+            itemView.configure(with: $0)
+            stackView.addArrangedSubview(itemView)
+        }
     }
     
+}
+
+extension StatsView {
+    override func setupViews() {
+        super.setupViews()
+
+        setupView(stackView)
+    }
+
+    override func constaintViews() {
+        super.constaintViews()
+
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 10),
+            stackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 15),
+            stackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -15),
+            stackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -10),
+        ])
+    }
+
+    override func configureAppearance() {
+        super.configureAppearance()
+    }
 }
